a168391d159e17cdeb23787e1ad8163921bc0a97
Michael Brummerhop <brumhop@gmail.com>
2022-12-04 20:35:50 -0600

    Day 3!

---
+++ src/AdventOfCode2022/Day03.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2022;
+public static class Day03
+{
+    public static int One(string path) => File.ReadAllLines(path)
+        .Select(l => l[0..(l.Length / 2)].Intersect(l[(l.Length / 2)..]).Single())
+        .Select(c => GetPriority(c))
+        .Sum();
+    
+    public static int Two(string path) => File.ReadAllLines(path)
+            .Select((l, i) => (Line: l, GroupNumber: i / 3))
+            .GroupBy(x => x.GroupNumber)
+            .Select(g => g.Select(x => x.Line.AsEnumerable())
+                .Aggregate((current, next) => current.Intersect(next))
+                .Single())
+            .Select(c => GetPriority(c))
+            .Sum();
+
+    private static int GetPriority(char item) => char.IsUpper(item) ? item - 38 : item - 96;
+}
--- src/AdventOfCode2022/Program.cs
+++ src/AdventOfCode2022/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AdventOfCode2022;
 
-Day01.One("Input\\day01.txt").WriteLine();
-Day01.Two("Input\\day01.txt").WriteLine();
-Day02.One("Input\\day02.txt").WriteLine();
-Day02.Two("Input\\day02.txt").WriteLine();
+//Day01.One("Input\\day01.txt").WriteLine();
+//Day01.Two("Input\\day01.txt").WriteLine();
+//Day02.One("Input\\day02.txt").WriteLine();
+//Day02.Two("Input\\day02.txt").WriteLine();
+//Day03.One("Input\\day03.txt").WriteLine();
+Day03.Two("Input\\day03.txt").WriteLine();
