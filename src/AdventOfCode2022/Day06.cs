9f1266ebeb6fb33efab91ecfd3f05b6de33f6ac5
Michael Brummerhop <michaelb@campusfederal.org>
2022-12-06 11:29:52 -0600

    Simplified Day 6.

--- src/AdventOfCode2022/Day06.cs
+++ src/AdventOfCode2022/Day06.cs
@@ -5,6 +5,5 @@ public static class Day06
     public static int Two(string path) => File.ReadAllText(path).Map(s => GetCharactersProcessed(s, 14));
     private static int GetCharactersProcessed(string buffer, int markerLength) => Enumerable.Range(0, buffer.Length - markerLength)
         .Select(x => (Start: x, Marker: buffer[x..(x + markerLength)]))
-        .Where(x => x.Marker.Distinct().Count() == markerLength)
-        .First().Start + markerLength;
+        .First(x => x.Marker.Distinct().Count() == markerLength).Start + markerLength;
 }
