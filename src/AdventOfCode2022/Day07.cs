3e8d5e9cea712b5f1b791c89e6516befa729682b
Michael Brummerhop <michaelb@campusfederal.org>
2022-12-14 21:20:38 -0600

    Day 8!

---
+++ src/AdventOfCode2022/Day07.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AdventOfCode2022;
+public static class Day07
+{
+    private static Dictionary<string, List<(string FileName, int Size)>> fileSystem = new();
+
+    public static void One(string path)
+    {
+        string[] terminalOutput = File.ReadAllLines(path);
+        //var x = terminalOutput
+        //    .Select((o, i) => (Output: o, Index: i))
+        //    .Where(o => o.Output.StartsWith("$ cd") && !o.Output.EndsWith(".."))
+        //    .Select(o => (Directory: o.Output.Split(' ')[^1], Size: Solve(terminalOutput, 0, o.Index)));
+        var results = new List<(string directory, int size)>();
+        var cds = terminalOutput
+            .Select((o, i) => (Output: o, Index: i))
+            .Where(o => o.Output.StartsWith("$ cd") && !o.Output.EndsWith("..")).ToList();
+        foreach (var cd in cds)
+        {
+            results.Add((cd.Output.Split(' ')[^1], Solve(terminalOutput, 0, cd.Index)));
+        }
+        Console.WriteLine(); 
+    }
+
+    private static int Solve(string[] terminalOutput, int level, int index)
+    {
+        int size = 0;
+        string command = terminalOutput[index];
+        if (level < 0 || index >= terminalOutput.Length)
+            return size;
+
+        if (terminalOutput[index] == "$ ls" || terminalOutput[index].StartsWith("dir"))
+            size += Solve(terminalOutput, level, ++index);
+
+        if (terminalOutput[index] == "$ cd ..")
+            size += Solve(terminalOutput, --level, ++index);
+
+        if (terminalOutput[index].StartsWith("$ cd") && terminalOutput[index].Split(' ')[^1] != "..")
+            size += Solve(terminalOutput, ++level, ++index);
+
+        if (char.IsDigit(terminalOutput[index][0]))
+        {
+            size += int.Parse(terminalOutput[index].Split(' ')[0]);
+            size += Solve(terminalOutput, level, ++index);
+        }
+
+        return size;
+    }
+
+    private static int AddTwo(int x) => x + 2;
+}
---
+++ src/AdventOfCode2022/Day08.cs
@@ -0,0 +1,86 @@
+﻿namespace AdventOfCode2022;
+public static class Day08
+{
+    private record struct Direction(int Row, int Col);
+
+    private static Direction[] directions = new[] { new Direction(-1, 0), new Direction(1, 0), new Direction(0, 1), new Direction(0, -1) };
+
+    private record struct Tree(int Height, bool Visible, int ScenicScore);
+
+    private static Tree[][] GetTrees(string path) => File.ReadAllLines(path)
+            .Select(r => r.Select(c => new Tree(c - '0', false, 0)).ToArray())
+            .ToArray();
+
+    public static int One(string path)
+    {
+        var trees = GetTrees(path);
+
+        for (int i = 0; i < trees.Length; i++)
+            for (int j = 0; j < trees[0].Length; j++) 
+                trees[i][j].Visible = OnEdge(trees, i, j) || Visible(trees, i, j);
+            
+        return trees.SelectMany(t => t).Where(t => t.Visible).Count();
+    }
+
+    public static int Two(string path)
+    {
+        var trees = GetTrees(path);
+
+        for (int i = 0; i < trees.Length; i++)
+            for (int j = 0; j < trees[0].Length; j++)
+                trees[i][j].ScenicScore = CalculateScenicScore(trees, i, j);
+
+        return trees.SelectMany(t => t).Max(t => t.ScenicScore);
+    }
+
+    private static bool Visible(Tree[][] trees, int row, int col)
+    {
+        bool visible = false;
+
+        foreach (var d in directions)
+        {
+            int nextRow = row + d.Row;
+            int nextCol = col + d.Col;
+            visible = true;
+
+            while (InBounds(trees, nextRow, nextCol) && visible)
+            {
+                visible = trees[row][col].Height > trees[nextRow][nextCol].Height;
+                nextRow = nextRow + d.Row;
+                nextCol = nextCol + d.Col;
+            }
+
+            if (visible)
+                break;
+        }
+        return visible;
+    }
+
+    private static int CalculateScenicScore(Tree[][] trees, int row, int col)
+    {
+        int score = 1;
+        foreach (var d in directions)
+        {
+            int nextRow = row + d.Row;
+            int nextCol = col + d.Col;
+            int distance = 0;
+
+            while (InBounds(trees, nextRow, nextCol))
+            {
+                distance++;
+
+                if (!(trees[row][col].Height > trees[nextRow][nextCol].Height))
+                    break;
+
+                nextRow = nextRow + d.Row;
+                nextCol = nextCol + d.Col;
+            }
+
+            score *= distance;
+        }
+        return score;
+    }
+
+    private static bool InBounds(Tree[][] trees, int row, int col) => row >= 0 && row < trees.Length && col >= 0 && col < trees[0].Length;
+    private static bool OnEdge(Tree[][] trees, int row, int col) => row == 0 || col == 0 || row == trees.Length - 1 || col == trees[0].Length - 1;
+}
